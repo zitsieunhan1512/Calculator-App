fdd0c9d76d7bf6e06cac66bbd209844b1a451145
LAPTOP-T22DSR93\admin <quanmtfu@gmail.com>
2021-12-08 13:38:37 +0700

    Calculator App

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput= findViewById(R.id.tvInput)
+    }
+
+    fun onDigit(view : View){
+        tvInput?.append((view as Button).text) 
     }
 }
